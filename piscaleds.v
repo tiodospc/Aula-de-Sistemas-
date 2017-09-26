6f4c0ad51f439b1167aa4751e0b9ade94acbfa36
tiodospc <thiago12812@gmail.com>
2017-09-26 15:15:00 -0300

    Trabalhos

---
+++ piscaleds.v
@@ -0,0 +1,27 @@
+// Quick blinking a LED
+
+/* module */
+module blinking (
+
+    input CLOCK_50,
+    output [1:0] LEDG
+    
+);
+
+    /* reg */
+    reg data1 = 1'b1;
+    reg [32:0] counter;
+    reg state;
+    
+    /* assign */
+    assign LEDG[0] = state;
+    assign LEDG[1] = data1;
+    
+    /* always */
+    always @ (posedge CLOCK_50) begin
+        counter <= counter + 1;
+        state <= counter[20]; // <------ data to change
+    end
+
+endmodule
+
